7442332998324739f461a2e10e64e6954386aed2
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2021-08-21 14:40:30 +0100

    Ping user when item completed

--- server/src/api/complete.ts
+++ server/src/api/complete.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from "express";
 import CraftingRequest, {ICraftingRequest} from "../models/request";
+import User from "../models/user";
+import bot from "../bot";
 
 export default async (req: Request, res: Response) => {
     const request: ICraftingRequest = req.body
@@ -9,4 +11,11 @@ export default async (req: Request, res: Response) => {
     }
     await CraftingRequest.deleteOne({_id: request._id})
     res.sendStatus(200)
+
+    const requester = await User.findOne({_id: request.madeBy})
+    const guild = await bot.guilds.fetch(requester.guildID)
+    const requesterDiscord = await guild.members.fetch(requester.discordID)
+    const crafterDiscord = await guild.members.fetch(res.locals.user.discordID)
+    const dmChannel = await requesterDiscord.createDM()
+    await dmChannel.send(`Your ${request.item} has been made, please contact ${crafterDiscord.displayName} to collect it!`)
 }
