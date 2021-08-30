173ead23adfa3a65a65ccf334d5699e4f597c677
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2021-08-30 21:24:01 +0100

    Add jewelry to the filter

--- server/src/api/itemNames.ts
+++ server/src/api/itemNames.ts
@@ -2,7 +2,7 @@ import {Request, Response} from "express";
 import {getCraftingData} from "../types/crafting";
 
 const {recipes} = getCraftingData()
-const whitelist = ["weapons", "armor", "tools", "concoctions", "consumables", "bags", "foods"]
+const whitelist = ["weapons", "armor", "tools", "concoctions", "consumables", "bags", "foods", "trinkets"]
 
 export default async (req: Request, res: Response) => {
     res.json(recipes.filter(recipe => whitelist.includes(recipe.itemType)).map(recipe => recipe.itemName))
