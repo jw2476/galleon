8a274d2a8f6e5bdc2fe3ec3a6b030332d2cb191b
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2021-10-09 11:11:02 +0100

    Add furniture

--- server/src/api/itemNames.ts
+++ server/src/api/itemNames.ts
@@ -2,7 +2,7 @@ import {Request, Response} from "express";
 import {getCraftingData} from "../types/crafting";
 
 const recipes = getCraftingData()
-const whitelist = ["weapons", "armor", "tools", "concoctions", "consumables", "bags", "foods", "trinkets"]
+const whitelist = ["weapons", "armor", "tools", "concoctions", "consumables", "bags", "foods", "trinkets", "furniture", "cutgems"]
 
 export default async (req: Request, res: Response) => {
     res.json(recipes.filter(recipe => whitelist.includes(recipe.itemType) && !recipe.bindOnPickup).map(recipe => recipe.itemName))
